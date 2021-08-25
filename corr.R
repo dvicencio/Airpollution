e90f6894714e199db7fbaab5f7eb3299fdf9a4e8
dvicencio <dvicencio947@gmail.com>
2021-08-25 14:52:21 -0500

    Initial Commit

--- corr.R
+++ corr.R
@@ -2,17 +2,27 @@ corr <- function(directory, threshold = 0) {
   
   files_full <- list.files(directory, full.names = TRUE)
   
-  dat <- data.frame()
+  vec <- vector()
   
   for (i in 1:332){
     
     A <- read.csv(files_full[i])
     B <- complete.cases(A)
     C <- sum(as.numeric(B))
+    
     if(C > threshold){
-    dat <- rbind(dat, C)
-}
+      D <- read.csv(files_full[i])
+      data_complete <- D[complete.cases(D), ]
+      
+      sul <- as.numeric(unlist(c(data_complete[2])))
+      nit <- as.numeric(unlist(c(data_complete[3])))
+      
+      E <- (cor(sul, nit))
+      vec <- rbind(vec, E)
+      
+    }
   }
-  print(dat)
+  done <- (c(vec))
+  return(done)
   
 }
--- test1.R
+++ test1.R
@@ -2,26 +2,19 @@ corr1 <- function(directory, threshold = 0) {
   
   files_full <- list.files(directory, full.names = TRUE)
   
-  vec <- vector()
+  dat <- data.frame()
   
   for (i in 1:332){
     
     A <- read.csv(files_full[i])
     B <- complete.cases(A)
     C <- sum(as.numeric(B))
-    
     if(C > threshold){
-      D <- read.csv(files_full[i])
-      data_complete <- D[complete.cases(D), ]
-      
-     sul <- as.numeric(unlist(c(data_complete[2])))
-     nit <- as.numeric(unlist(c(data_complete[3])))
-     
-     E <- (cor(sul, nit))
-     vec <- rbind(vec, E)
-      
+      dat <- rbind(dat, C)
     }
   }
- print(c(vec))
+  print(dat)
   
 }
+
+  
